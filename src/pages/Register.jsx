88261505369a4170867cfc97a0ba379ccaa43d24
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-10-03 14:02:19 +0100

    changed the url

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -13,7 +13,7 @@ export default function Register({ onRegister }) {
 
   const submit = async (e) => {
     e.preventDefault();
-    const res = await fetch(`${API}register`, {
+    const res = await fetch(`${API}/api/auth/register`, {
       method:'POST',
       headers:{'Content-Type':'application/json'},
       body: JSON.stringify({ name, email, password, role })
